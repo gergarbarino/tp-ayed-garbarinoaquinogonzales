5168423c7fcf9422d6e9e85e9c2735532551184e
German Garbarino <168428299+gergarbarino@users.noreply.github.com>
2026-09-11 20:31:16 -0300

    Update main.py

--- tp-integrador/esqueleto/src/dominio/main.py
+++ tp-integrador/esqueleto/src/dominio/main.py
@@ -1,3 +1,21 @@
 from catalogo import cargar_catalogo
 
-catalogo = cargar_catalogo("pokedex.csv")
+
+def main():
+    catalogo = cargar_catalogo("docs/data/pokedex.csv")
+
+    while True:
+        entrada = input("Ingrese el ID del Pokémon o 'apagar' para salir: ")
+
+        if entrada.lower() == "apagar":
+            break
+
+        id_buscado = int(entrada)
+
+        for pokemon in catalogo:
+            if pokemon.numero == id_buscado:
+                print(pokemon)
+
+
+if __name__ == "__main__":
+    main()
