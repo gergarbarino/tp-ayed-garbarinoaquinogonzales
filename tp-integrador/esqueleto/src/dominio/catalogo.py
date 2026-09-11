37fa672df1fdd2e0076c7efc641347ffbe89d9cd
German Garbarino <168428299+gergarbarino@users.noreply.github.com>
2026-09-11 20:31:31 -0300

    Update catalogo.py

--- tp-integrador/esqueleto/src/dominio/catalogo.py
+++ tp-integrador/esqueleto/src/dominio/catalogo.py
@@ -2,13 +2,33 @@ import csv
 from pokemon import Pokemon
 
 
-def cargar_catalogo(pokedex):
+def cargar_catalogo(nombre_archivo):
     pokemons = []
 
-    with open(pokedex, "r") as archivo:
+    with open(nombre_archivo, "r", encoding="utf-8") as archivo:
         lector = csv.reader(archivo)
 
+        next(lector)  # Saltea la primera fila, que es el encabezado
+
         for fila in lector:
-            print(fila)
+            numero = int(fila[0])
+            nombre = fila[1]
+            tipo = fila[2]
+            hp = int(fila[4])
+            ataque = int(fila[5])
+            defensa = int(fila[6])
+            velocidad = int(fila[7])
+
+            pokemon = Pokemon(
+                numero,
+                nombre,
+                tipo,
+                hp,
+                ataque,
+                defensa,
+                velocidad
+            )
+
+            pokemons.append(pokemon)
 
     return pokemons
